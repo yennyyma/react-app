da19094ef8403ede613f4d1d3c721a2e4542a181
yennyyma <mayennyy@gmail.com>
2025-04-11 23:01:14 -0700

    Completed IE1: Step 10

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
     const [person, setPerson] = useState ({
         name: "",
         job: ""
     });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        if (name === "job") {
+            setPerson({ name: person["name"], job: value });
+        }
+        else {
+            setPerson({ name: value, job: person["job"]});
+        }
+    }
+
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({name: "", job: ""});
+    }
 
     return (
         <form>
@@ -19,23 +34,18 @@ function Form() {
             <label htmlFor="job">Job</label>
             <input
                 type="text"
-                job="job"
+                name="job"
                 id="job"
                 value={person.job}
                 onChange={handleChange}
             />
+            <input
+                type="button"
+                value="Submit"
+                onClick={submitForm}
+            />
         </form>
     );
-}
-
-function handleChange(event) {
-    const { name, value } = event.target;
-    if (name === "job") {
-        setPerson({ name: person["name"], job: value });
-    }
-    else {
-        setPerson({ name: value , job: person["job"] });
-    }
 }
 
 export default Form;
