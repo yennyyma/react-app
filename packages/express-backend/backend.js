e9140fb78f1b3760bcd684c8b5c7673b607685d6
yennyyma <mayennyy@gmail.com>
2025-04-18 10:37:21 -0700

    Completed step 4 of IE2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -31,11 +31,23 @@ const users = {
         }
     ]
 };
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
 
 app.use(express.json());
 
 app.get("/users", (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    } else {
+        res.send(users);
+    }
 });
 
 app.listen(port, () => {
