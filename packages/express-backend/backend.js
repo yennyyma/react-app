62d1ebe83b9f495cd3eb4973bb9905df67a73798
yennyyma <mayennyy@gmail.com>
2025-04-22 20:02:37 -0700

    Updated step 2 of IE3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -44,8 +45,9 @@ const findUserByJob = (job) => {
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
 
-
+app.use(cors());
 app.use(express.json());
+
 const addUser = (user) => {
     users["users_list"].push(user);
     return users;
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -16,6 +16,20 @@ function MyApp() {
   function updateList(person) {
     setCharacters([...characters, person]);
   }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="container">
